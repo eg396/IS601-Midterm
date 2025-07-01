a327f6a5983f1eccddd6dea278301c17acf5ee81
Evan <eg396@njit.edu>
2025-07-01 15:43:06 -0400

    chore: add logger to input validators file

--- app/input_validators.py
+++ app/input_validators.py
@@ -7,6 +7,7 @@ from decimal import Decimal, InvalidOperation
 from typing import Any
 from app.calculator_config import CalculatorConfig
 from app.exceptions import ValidationError
+from app.logger import CalculationLogger
 
 @dataclass
 class InputValidator:
@@ -14,8 +15,12 @@ class InputValidator:
     ## InputValidator class
     ## contains static methods for validating user input
 
+    def __init__(self):
+
+        self.logger = CalculationLogger()
+
     @staticmethod
-    def validate_input(value: Any, config: CalculatorConfig) -> Decimal:
+    def validate_input(self, value: Any, config: CalculatorConfig) -> Decimal:
 
         ## Validate the input and convert it to Decimal
 
@@ -44,16 +49,19 @@ class InputValidator:
             ## Check if the value is greater than the maximum allowed value
             ## If so, raise a ValidationError
 
-            if abs(number) > config.max_input_val:\
+            if abs(number) > config.max_input_val:
 
-                raise ValidationError(f"Value exceeds maximum allowed value of {config.max_input_val}")
+                self.logger.log_error(f"Value {number} exceeds maximum allowed value of {config.max_input_val}")
+                raise ValidationError
             
             ## Return the normalized number
 
+            self.logger.log_info(f"Validated input: {number}")
             return number.normalize()
         
         ## If the operation is not valid, raise a ValidationError
         
         except InvalidOperation as e:
 
-            raise ValidationError(f"Invalid number format: {e}") from e
+            self.logger.log_error(f"Invalid number format: {e}")
+            raise ValidationError
